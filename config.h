419c7364c0cc45cb77835e8adf761e8c72bb6abe
mamizaxyz <mamizaxyz@protonmail.com>
2021-11-06 18:26:06 +0330

    Initial commit

---
+++ config.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",           "sb-capslock",		1,			9},
+	{"",           "sb-pacup",		21600,			8},
+	{"",           "sb-cpu",		10,			18},
+	{"",           "sb-memory",		10,			14},
+	{"",           "sb-volume",		0,			10},
+	{"",           "sb-battery",		5,			3},
+	{"",           "sb-internet",		5,			4},
+	{"",           "sb-clock",		60,			1},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char *delim = " | ";
