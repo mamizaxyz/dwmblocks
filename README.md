ed4cc20611f640233ff6e6bbf8d929f5feb819ee
Mamiza <themamiza@gmail.com>
2022-07-29 16:36:54 +0430

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# dwmblocks
-Mamiza's Custom Build of Dwmblocks
+# Dwmblocks
+
+Mamiza's Custom Build of Dwmblocks (Modular statusbar for dwm written in C).
+
+# Modifying blocks
+
+The statusbar is made from text output from command-line programs. Blocks are added and removed by editing the `blocks.def.h` file.
+
+# Mamiza's build
+
+This build of dwmblocks will default to my scripts in my dotfiles, so if you want it to read your own scripts
+edit `blocks.def.h`.
+
+# Signaling changes
+
+You can signal updates to your specific block with `kill -(signum) $(pidof dwmblocks)`, signum is the block's update signal +34.
+
+Note that all the blocks should have their own update signal.
+
+# Clickable modules
+
+This build will send your mouse input to your script as the `$BLOCK_BUTTON` variable.
+
+For this to work you also need the appropriate patch installed on your dwm build.
---
+++ blocks.def.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+      /*Icon*/  /*Command*/     /*Update Interval*/  /*Update Signal*/
+
+    { "",       "sb-pacup",     3600,                8  },
+    /* { "",       "sb-lastup",    86400,               8  }, */
+    { "",       "sb-uptime",    60,                 12  },
+    { "",       "sb-volume",    0,                  10  },
+    { "",       "sb-battery",   5,                   3  },
+    { "",       "sb-nettraf",   1,                  16  },
+    { "",       "sb-internet",  5,                   4  },
+    { "",       "sb-clock",     60,                  1  },
+    { "",       "sb-xkbmap",    0,                  15  },
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char *delim = " / ";
